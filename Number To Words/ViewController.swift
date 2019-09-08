35d7459e43e0a70d300275438779e1a4fbbe7f54
Jobin Basani <jobinbasani@gmail.com>
2019-09-08 07:19:07 -0300

    Add event handlers

--- Number To Words/ViewController.swift
+++ Number To Words/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    @IBOutlet weak var buttonContainer: UIStackView!
+    
 
+    //MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for numberConfigButton in getNumberConfigButtons(){            
+            numberConfigButton.addTarget(self, action: #selector(onButtonClick(sender:)), for: .touchUpInside)
+        }
     }
-
+    //MARK: - Event handlers
+    @objc func onButtonClick(sender: NumberConfigButton){
+        print(sender.titleLabel?.text ?? "No Title")
+    }
+    //MARK: - Private functions
+    private func getChildViewsOf<T: UIView>(view: UIView) -> [T] {
+        var subviews = [T]()
+        
+        for subview in view.subviews {
+            subviews += getChildViewsOf(view: subview) as [T]
+            if let subview = subview as? T{
+                subviews.append(subview)
+            }
+        }
+        return subviews
+    }
+    
+    private func getNumberConfigButtons() -> [NumberConfigButton] {
+        return getChildViewsOf(view: buttonContainer)
+    }
 
 }
 
