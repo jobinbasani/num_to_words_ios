0800129a042584bc41dee533a55202731a3014d2
Jobin Basani <jobinbasani@gmail.com>
2019-09-08 04:26:14 -0300

    initial commit

---
+++ Number To Words/NumberConfigButton.swift
@@ -0,0 +1,16 @@
+//
+//  NumberConfigButton.swift
+//  Number To Words
+//
+//  Created by Jobin Basani on 2019-09-08.
+//  Copyright © 2019 Jobin Basani. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class NumberConfigButton: UIButton {
+
+    //MARK: - Properties
+    @IBInspectable var isNumberButton:Bool = true
+
+}
